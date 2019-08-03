233833f5835c33020f09effd942579f5b8938150
Darkangel <wtsegars@mac.com>
2019-08-02 22:52:27 -0400

    Made minor updates to server.js

--- server.js
+++ server.js
@@ -2,3 +2,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "login.html"));
+});
+
+app.get("/main-menu", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
