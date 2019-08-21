29008bf9870a508e28bee33129430e8735e06fc3
Darkangel <wtsegars@mac.com>
2019-08-21 18:13:58 -0400

    Updated Express Routes

--- server.js
+++ server.js
@@ -14,6 +14,34 @@ app.get("/main-menu", function(req, res) {
     res.sendFile(path.join(__dirname, "main-menu.html"));
 });
 
+app.get("/main-menu/store-leads-week", function(req, res) {
+    res.sendFile(path.join(__dirname, "store-week.html"));
+});
+
+app.get("/main-menu/store-leads-month", function(req, res) {
+    res.sendFile(path.join(__dirname, "store-month.html"));
+});
+
+app.get("/main-menu/store-leads-year", function(req, res) {
+    res.sendFile(path.join(__dirname, "store-year.html"));
+});
+
+app.get("/main-menu/department-leads-week", function(req, res) {
+    res.sendFile(path.join(__dirname, "dept-week.html"));
+});
+
+app.get("/main-menu/department-leads-month", function(req, res) {
+    res.sendFile(path.join(__dirname, "dept-month.html"));
+});
+
+app.get("/main-menu/department-leads-year", function(req, res) {
+    res.sendFile(path.join(__dirname, "dept-year.html"));
+});
+
+app.get("/main-menu/update-leads", function(req, res) {
+    res.sendFile(path.join(__dirname, "update-leads.html"));
+});
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
