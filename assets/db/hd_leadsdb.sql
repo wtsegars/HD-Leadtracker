dbc81477628f4dbe7cc71e3d00dbaaed56843e4f
Darkangel <wtsegars@mac.com>
2019-08-28 16:09:29 -0400

    Added Mongoose

--- assets/db/hd_leadsdb.sql
+++ assets/db/hd_leadsdb.sql
@@ -4,7 +4,6 @@ CREATE DATABASE hd_leadsdb;
 
 USE hd_leadsdb;
 
-CREATE TABLE hd_leads_weeks(
-	start_date TIMESTAMP(8) NOT NULL,
-    end_date TIMESTAMP('%m' '%d' '%Y') NOT NULL
+CREATE TABLE hd_leads_2019(
+
 );
