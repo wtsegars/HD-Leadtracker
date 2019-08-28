deb2bbf69864252680a6d2c9ae4739bb5fe39822
Darkangel <wtsegars@mac.com>
2019-08-28 16:54:55 -0400

    MySQL Added back

---
+++ db/hd_leadsdb.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS hd_leads_1755;
+
+CREATE DATABASE hd_leads_1755;
+
+USE hd_leads;
+
+CREATE TABLE hd_leads_2019(
+	depts VARCHAR(6) NOT NULL,
+    lead_goals INT(4),
+    total_leads INT(4)
+);
+
+CREATE TABLE hd_leads_jan2019(
+	depts VARCHAR(6) NOT NULL,
+    lead_goals INT(4),
+    total_leads INT(4)
+);
+
+CREATE TABLE hd_leads_jan2019_week1(
+	depts VARCHAR(6) NOT NULL,
+    dates DATE NOT NULL,
+    lead_goals INT(4),
+    total_leads INT(4)
+);
