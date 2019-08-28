c87940aa410618c7e0f8238e1f9788ec728dfa85
Darkangel <wtsegars@mac.com>
2019-08-28 19:32:58 -0400

    Minor Updates

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,44 @@
 INSERT INTO hd_leads_2019 (departments)
 VALUES ("D21/22"); 
+
+INSERT INTO hd_leads_2019 (departments)
+VALUES ("D23/59");
+
+INSERT INTO hd_leads_2019 (departments)
+VALUES ("D24");
+
+INSERT INTO hd_leads_2019 (departments)
+VALUES ("D25");
+
+INSERT INTO hd_leads_2019 (departments)
+VALUES ("D26/85");
+
+INSERT INTO hd_leads_2019 (departments)
+VALUES ("D27");
+
+INSERT INTO hd_leads_2019 (departments)
+VALUES ("D28");
+
+INSERT INTO hd_leads_2019 (departments)
+VALUES ("D29/70");
+
+INSERT INTO hd_leads_2019 (departments)
+VALUES ("D30");
+
+INSERT INTO hd_leads_2019 (departments)
+VALUES ("D31/94");
+
+INSERT INTO hd_leads_2019 (departments)
+VALUES ("D42");
+
+INSERT INTO hd_leads_2019 (departments)
+VALUES ("D78");
+
+INSERT INTO hd_leads_2019 (departments)
+VALUES ("D90/96");
+
+INSERT INTO hd_leads_2019 (departments)
+VALUES ("D93/38");
+
+INSERT INTO hd_leads_2019 (departments)
+VALUES ("D01");
