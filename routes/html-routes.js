b6f2f6e47259b0574ad349be9e8580d8620b0b5d
Darkangel <wtsegars@mac.com>
2019-09-19 22:55:06 -0400

    Added authentication to other routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -14,35 +14,35 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../pages/main-menu.html"));
     });
 
-    app.get("/main-menu/store-week-leads", function(req, res) {
+    app.get("/main-menu/store-week-leads", isAuthenticated, function(req, res) {
         res.sendFile(path.join(__dirname, "../pages/store-week.html"));
     });
 
-    app.get("/main-menu/store-month-leads", function(req, res) {
+    app.get("/main-menu/store-month-leads", isAuthenticated, function(req, res) {
         res.sendFile(path.join(__dirname, "../page/store-month.html"));
     });
 
-    app.get("/main-menu/store-year-leads", function(req, res) {
+    app.get("/main-menu/store-year-leads", isAuthenticated, function(req, res) {
         res.sendFile(path.join(__dirname, "../pages/store-year.html"));
     });
 
-    app.get("/main-menu/department-week-leads", function(req, res) {
+    app.get("/main-menu/department-week-leads", isAuthenticated, function(req, res) {
         res.sendFile(path.join(__dirname, "../page/dept-week.html"));
     });
 
-    app.get("/main-menu/department-month-leads", function(req, res) {
+    app.get("/main-menu/department-month-leads", isAuthenticated, function(req, res) {
         res.sendFile(path.join(__dirname, "../pages/dept-month.html"));
     });
 
-    app.get("/main-menu/department-year-leads", function(req, res) {
+    app.get("/main-menu/department-year-leads", isAuthenticated, function(req, res) {
         res.sendFile(path.join(__dirname, "../pages/dept-year.html"));
     });
 
-    app.get("/main-menu/update-leads", function(req, res) {
+    app.get("/main-menu/update-leads", isAuthenticated, function(req, res) {
         res.sendFile(path.join(__dirname, "../pages/update-leads.html"));
     });
 
-    app.get("/main-menu/set-goals", function(req, res) {
+    app.get("/main-menu/set-goals", isAuthenticated, function(req, res) {
         res.sendFile(path.join(__dirname, "../pages/set-goals.html"));
     });
 };
